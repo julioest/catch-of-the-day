a0bb6cbdf2f591241c923dde8ac0e83947f9aa33
Julio Estrada <info@julioest.com>
2016-07-06 00:08:23 -0400

    Latest code. Left off @ 17: Bi-Directional Data Flow with LinkState

--- scripts/main.js
+++ scripts/main.js
@@ -10,15 +10,47 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
 
 /*
+  Firebase
+*/
+
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catch-of-the-day-1f506.firebaseio.com/');
+
+var Catalyst = require('react-catalyst');
+
+/*
   App
 */
 
 var App = React.createClass({
+  mixins : [Catalyst.LinkedStateMixin],
   getInitialState : function(){
     return {
       fishes : {},
       order : {}
     }
+  },
+  componentDidMount : function(){
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
+
+    var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if (localStorageRef) {
+      // update our component state to reflect what is in the localStorage
+      this.setState({
+        order : JSON.parse(localStorageRef)
+      });
+    }
+  },
+  componentWillUpdate : function(nextProps, nextState){
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
+  },
+  addToOrder : function(key){
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({ order: this.state.order });
   },
   addFish : function(fish){
     var timestamp = (new Date()).getTime();
@@ -27,20 +59,63 @@ var App = React.createClass({
     // set the state
     this.setState({ fishes : this.state.fishes });
   },
+  removeFish : function(key){
+    if (confirm("Are you sure you want to remove this fish?")){
+      this.state.fishes[key] = null;
+      this.setState({
+        fishes : this.state.fishes
+      });
+    }
+  },
   loadSamples : function(){
     this.setState({
       fishes : require('./sample-fishes')
     });
+  },
+  renderFish : function(key){
+    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
   },
   render : function(){
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes).map(this.renderFish)}
+          </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+      <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish}/>
       </div>
+    )
+  }
+});
+
+/*
+  Fish
+  <Fish />
+*/
+
+var Fish = React.createClass({
+  onButtonClick : function(){
+    console.log("Going to add the Fish: ", this.props.index);
+    var key = this.props.index;
+    this.props.addToOrder(key);
+  },
+  render : function(){
+    var details = this.props.details;
+    var isAvailable = (details.status === 'available' ? true : false);
+    var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+      <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
+      </li>
     )
   }
 });
@@ -113,9 +188,48 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
-  render : function(){
+  renderOrder : function(key){
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if(!fish){
+        return <li key={key}>Sorry, fish no longer available!</li>
+    }
+
     return (
-      <p>Order</p>
+      <li key={key}>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
+  render : function(){
+    var orderIds = Object.keys(this.props.order);
+
+    var total = orderIds.reduce((prevTotal, key)=>{
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if(fish && isAvailable){
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {h.formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     )
   }
 });
@@ -126,12 +240,31 @@ var Order = React.createClass({
 */
 
 var Inventory = React.createClass({
+  renderInventory : function(key){
+    var linkState = this.props.linkState;
+    return (
+      <div className="fish-edit" key={key}>
+          <input type="text" valueLink={linkState('fishes.' + key + '.name')}/>
+          <input type="text" valueLink={linkState('fishes.' + key + '.price')}/>
+        <select valueLink={linkState('fishes.'+ key + '.status')}>
+          <option value="unavailable">Sold Out!</option>
+          <option value="available">Fresh!</option>
+        </select>
+        <textarea valueLink={linkState('fishes.'+ key + '.desc')}></textarea>
+        <input type="text" valueLink={linkState('fishes.' + key + '.image')}/>
+      <button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
+      </div>
+    )
+  },
   render : function(){
     return (
       <div>
         <h2>Inventory</h2>
+
+      {Object.keys(this.props.fishes).map(this.renderInventory)}
+
         <AddFishForm {...this.props} />
-        <button onClick={this.props.loadSamples}>Load sample fishes!</button>
+        <button onClick={this.props.loadSamples}>Load Sample Fishes!</button>
       </div>
     )
   }
